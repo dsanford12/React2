06c2cdc68ba3515ef378287daba1103cdfb00454
Dale Sanford <dsanford12@gmail.com>
2017-10-28 11:07:13 -0700

    finish project

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import CamperList from './camper_list';
+
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentCampers: [],
+      allTimeCampers: [],
+      currentView: 'recentCampers'
+    };
+  }
+
+  componentWillMount() {
+    axios.all([this.fetchRecentCampers(),this.fetchAllTimeCampers()])
+      .then(axios.spread((recentCampers, allTimeCampers) => {
+        this.setState({
+          recentCampers: recentCampers.data,
+          allTimeCampers: allTimeCampers.data
+        });
+      }));
+  }
+//TODO:  Look at this shit!
+  fetchRecentCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+
+  }
+
+  fetchAllTimeCampers() {
+    return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+
+  }
+
+  changeView(currentView) {
+    this.setState({currentView});
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <h2>{`Viewing Top ${this.state.currentView}`}</h2>
+        <button onClick={() => this.changeView('recentCampers')} className="btn btn-primary">Recent</button>
+        <button onClick={() => this.changeView('allTimeCampers')} className="btn btn-primary">All Time</button>
+        <CamperList campers={this.state[this.state.currentView]} />
+      </div>
     );
   }
 }
